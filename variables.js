185694a6ed2bc5a3a4bd8e220ad73c51d2ad3fca
Camelle <baloiscamelle@gmail.com>
2024-09-13 21:48:16 +0800

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,9 @@
+let name = "Camelle";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Female";
+let twitterHandle = "none";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
